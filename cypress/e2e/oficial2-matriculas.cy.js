a4672d05c468a60f68af0ce0f205fa64781d2be7
eldersb <122701368+eldersb@users.noreply.github.com>
2024-05-31 20:34:52 -0300

    Problemas no endpoint bolsista_50

--- cypress/e2e/oficial2-matriculas.cy.js
+++ cypress/e2e/oficial2-matriculas.cy.js
@@ -20,10 +20,12 @@ describe('template spec', () => {
       expect(response.status).to.eq(statusCode)
 
       expect(response.body).to.have.property('id') 
-      expect(typeof(response.body.id)).to.eq('string') // Esse id é uma string, verificar se é para ser mesmo uma string ou um number(int)
+      expect(typeof(response.body.id)).to.eq('string') 
 
       expect(response.body).to.have.property('courseName')
       expect(typeof(response.body.courseName)).to.eq('string')
+      expect(response.body.courseName.length).to.be.greaterThan(0)
+
 
       expect(response.body).to.have.property('tuition')
       expect(typeof(response.body.tuition)).to.eq('object')
@@ -38,20 +40,24 @@ describe('template spec', () => {
       expect(response.body.tuition).to.have.property('dueDate')
       expect(typeof(response.body.tuition.dueDate)).to.eq('string')
       expect(response.body.tuition.dueDate).to.be.include('-')
+      expect(response.body.tuition.dueDate.length).to.be.greaterThan(0)
   
       expect(response.body.tuition).to.have.property('status')
       expect(typeof(response.body.tuition.status)).to.eq('string')
       expect(response.body.tuition.status).to.be.include('_')
-
 
       expect(response.body).to.have.property('student')
       expect(typeof(response.body.student)).to.eq('object')
 
       expect(response.body.student).to.have.property('firstName')
       expect(typeof(response.body.student.firstName)).to.eq('string')
+      expect(response.body.student.firstName.length).to.be.greaterThan(0)
+
 
       expect(response.body.student).to.have.property('lastName')
       expect(typeof(response.body.student.lastName)).to.eq('string')
+      expect(response.body.student.lastName.length).to.be.greaterThan(0)
+
 
       expect(response.body.student).to.have.property('birthDate')
       expect(typeof(response.body.student.birthDate)).to.eq('string')
@@ -66,7 +72,7 @@ describe('template spec', () => {
 
     })
 
-    it('Deve ser possível retornar dados de matrícula válida e que está com pagamento atrasado', () => {
+  it('Deve ser possível retornar dados de matrícula válida e que está com pagamento atrasado', () => {
 
       const matriculas = "5566778"
 
@@ -91,7 +97,7 @@ describe('template spec', () => {
 
     })
 
-    it('Deve ser possível consultar os dados de uma matrícula de aluno bolsista 100% e obter os dados dessa matrícula', () => {
+  it('Deve ser possível consultar os dados de uma matrícula de aluno bolsista 100% e obter os dados dessa matrícula', () => {
 
       const matriculas = "7890123"
 
@@ -114,6 +120,8 @@ describe('template spec', () => {
 
         expect(response.body).to.have.property('courseName')
         expect(typeof(response.body.courseName)).to.eq('string')
+        expect(response.body.courseName.length).to.be.greaterThan(0)
+
 
         expect(response.body).to.have.property('tuition')
         expect(typeof(response.body.tuition)).to.eq('object')
@@ -138,9 +146,13 @@ describe('template spec', () => {
   
         expect(response.body.student).to.have.property('firstName')
         expect(typeof(response.body.student.firstName)).to.eq('string')
+        expect(response.body.student.firstName.length).to.be.greaterThan(0)
+
   
         expect(response.body.student).to.have.property('lastName')
         expect(typeof(response.body.student.lastName)).to.eq('string')
+        expect(response.body.student.lastName.length).to.be.greaterThan(0)
+
   
         expect(response.body.student).to.have.property('birthDate')
         expect(typeof(response.body.student.birthDate)).to.eq('string')
@@ -181,16 +193,18 @@ describe('template spec', () => {
 
         expect(response.body).to.have.property('courseName')
         expect(typeof(response.body.courseName)).to.eq('string')
+        expect(response.body.courseName.length).to.be.greaterThan(0)
 
         expect(response.body).to.have.property('tuition')
         expect(typeof(response.body.tuition)).to.eq('object')
 
         expect(response.body.tuition).to.have.property('amount')
-        expect(response.body.tuition.amount).to.eq(0)
+        expect(response.body.tuition.amount).to.eq('number')
+        expect(response.body.student.firstName.length).to.be.greaterThan(0)
 
         expect(response.body.tuition).to.have.property('formattedAmount')
         expect(typeof(response.body.tuition.formattedAmount)).to.eq('string')
-        expect(response.body.tuition.formattedAmount).to.be.include('R$ 0,00')
+        expect(response.body.tuition.formattedAmount).to.be.include('R$')
 
         expect(response.body.tuition).to.have.property('dueDate')
         expect(typeof(response.body.tuition.dueDate)).to.eq('object')
@@ -205,14 +219,19 @@ describe('template spec', () => {
         expect(typeof(response.body.student)).to.eq('object')
   
         expect(response.body.student).to.have.property('firstName')
-        expect(response.body.student.firstName).to.eq('')
+        expect(typeof(response.body.student.firstName)).to.eq('string')
+        expect(response.body.student.firstName.length).to.be.greaterThan(0)
   
         expect(response.body.student).to.have.property('lastName')
-        expect(response.body.student.lastName).to.eq('')
+        expect(typeof(response.body.student.lastName)).to.eq('string')
+        expect(response.body.student.lastName.length).to.be.greaterThan(0)
+
 
         expect(response.body.student).to.have.property('birthDate')
-        expect(typeof(response.body.student.birthDate)).to.eq('object')
-        expect(response.body.student.birthDate).to.eq(null)
+        expect(typeof(response.body.student.birthDate)).to.eq('string')
+        expect(response.body.student.birthDate).to.be.include('-')
+        expect(response.body.student.birthDate.length).to.be.greaterThan(0)
+
   
         expect(response.body.student).to.have.property('cpf')
         expect(typeof(response.body.student.cpf)).to.eq('string')
@@ -242,10 +261,12 @@ describe('template spec', () => {
         expect(response.status).to.eq(statusCode)
 
         expect(response.body).to.have.property('id') 
-        expect(typeof(response.body.id)).to.eq('string') // Esse id é uma string, verificar se é para ser mesmo uma string ou um number(int)
+        expect(typeof(response.body.id)).to.eq('string') 
 
         expect(response.body).to.have.property('courseName')
         expect(typeof(response.body.courseName)).to.eq('string')
+        expect(response.body.courseName.length).to.be.greaterThan(0)
+
 
         expect(response.body).to.have.property('tuition')
         expect(typeof(response.body.tuition)).to.eq('object')
@@ -270,9 +291,13 @@ describe('template spec', () => {
   
         expect(response.body.student).to.have.property('firstName')
         expect(typeof(response.body.student.firstName)).to.eq('string')
+        expect(response.body.student.firstName.length).to.be.greaterThan(0)
+
   
         expect(response.body.student).to.have.property('lastName')
         expect(typeof(response.body.student.lastName)).to.eq('string')
+        expect(response.body.student.lastName.length).to.be.greaterThan(0)
+
 
         expect(response.body.student).to.have.property('birthDate')
         expect(typeof(response.body.student.birthDate)).to.eq('string')
